84b1fb34b145f6b997036d3d21a17aec28fc107b
jungsunwoung <94wjdtjsdnd@naver.com>
2019-06-02 15:38:17 +0900

    almost done

---
+++ README.md
@@ -0,0 +1,5 @@
+# GooroomTeo
+모바일앱프로그래밍실습_구름터
+
+SKKU, Mobile App Programming Practice
+GooroomTeo, ¿Where is the smoking zone?
---
+++ app/src/main/java/edu/skku/GooroomTeo/MainActivity.java
@@ -0,0 +1,127 @@
+package edu.skku.GooroomTeo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.app.FragmentManager;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity
+        implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener{
+    Button addbtn;
+    Button findbtn;
+    GoogleMap map;
+    double longitude;
+    double latitude;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getFragmentManager();
+        MapFragment mapFragment = (MapFragment)fragmentManager
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        //현재위치에서 시작
+
+        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission(getApplicationContext(),
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]
+                    {android.Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        } else {
+
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                    100,
+                    0,
+                    networkLocationListener);
+        }
+
+        addbtn = findViewById(R.id.addcontent);
+        addbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findbtn = findViewById(R.id.findnear);
+        findbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LatLng MyPos = new LatLng(latitude, longitude);
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(MyPos);
+                markerOptions.title("현위치");
+                map.addMarker(markerOptions);
+                map.moveCamera(CameraUpdateFactory.newLatLng(MyPos));
+                map.animateCamera(CameraUpdateFactory.zoomTo(16));
+
+            }
+        });
+
+    }
+    final LocationListener networkLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            String provider = location.getProvider();
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+            Log.d("Update Network", "" + provider + " " + longitude + " " + latitude);
+
+        }
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+        @Override
+        public void onProviderEnabled(String provider) {}
+        @Override
+        public void onProviderDisabled(String provider) {}
+    };
+
+
+    @Override
+    public void onMapReady(final GoogleMap map) {
+        this.map = map;
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울");
+        markerOptions.snippet("한국의 수도");
+        map.addMarker(markerOptions);
+        map.setOnMarkerClickListener(this);
+        map.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        map.animateCamera(CameraUpdateFactory.zoomTo(10));
+    }
+
+    public boolean onMarkerClick(Marker marker){
+
+        Intent intent = new Intent(MainActivity.this, RateActivity.class);
+        String title=marker.getTitle();
+        intent.putExtra("information",title);
+        startActivity(intent);
+
+        return true;
+
+    }
+}
